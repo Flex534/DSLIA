2afc213563fab5fcf782fe23d0bcaafd162e0362
Fabri <fabricapo298@gmail.com>
2025-06-09 19:27:43 -0300

    Se asigne alumno a los que entren y docentes Prior

--- main.py
+++ main.py
@@ -6,9 +6,34 @@ import dst
 
 intents= discord.Intents.default()
 intents.message_content= True
+intents.members = True
+@bot.event
+async def on_member_join(member):
+    # Nombre del rol a asignar automáticamente
+    role_name = "Alumno"
+
+    # Buscar el rol por nombre en el servidor
+    role = discord.utils.get(member.guild.roles, name=role_name)
+
+    if role:
+        await member.add_roles(role)
+        print(f"Se asignó el rol '{role_name}' a {member.name}")
+    else:
+        print(f"Rol '{role_name}' no encontrado en el servidor.")
 
 bot= commands.Bot(command_prefix ="!", intents=intents)
 
+@bot.command()
+@commands.has_role("Docente")
+async def canal_privado(ctx):
+    await ctx.send("Este comando solo lo pueden usar docentes.")
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.MissingRole):
+        await ctx.send("❌ No tenés permisos para usar este comando.")
+    else:
+        raise error  # para que otros errores se muestren normalmente
 
 @bot.command()
 async def prueba(ctx,*args):
